708a2f42ee709394c5b99c77e65aa036a838501e
unknown <haren111990@gmail.com>
2026-03-26 12:02:11 +0530

    bg image

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@
 
 export default function Home() {
   return (
-    <div className="bg-gray-700 h-screen">
+    <div className="h-screen bg-center bg-cover"
+    style={{ backgroundImage: "url('/images/Ganeshji.jpg')" }}>
       <h1 className="text-red-600">Welcome to the Invitation Card</h1>
       <p>Harendrasinh</p>
     </div>
