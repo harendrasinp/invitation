590d4b7217434f14096b5eb88ae0ad91c5afaddc
unknown <haren111990@gmail.com>
2026-03-26 14:07:56 +0530

    title and button

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,13 @@
 
 export default function Home() {
   return (
-    <div className="h-screen w-full p-10">
-      <div className="h-80 w-full bg-cover bg-center" 
+    <div className="h-screen w-full p-5">
+      <div className="h-100 w-full bg-cover bg-center" 
       style={{ backgroundImage: "url('/images/ganeshji2.png')" }}></div>
-      <div className="flex flex-col items-center justify-center mt-10">
-        <h1 className="text-4xl font-bold text-purple-400 mb-4">VASTU POOJAN</h1>
-        <p className="text-lg text-green-500 mb-8">Join us for the inauguration of our new business.</p>
-        <a href="/rsvp" className="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-300">RSVP Now</a>
+      <div className="flex flex-col items-center justify-center mt-1">
+        <h1 className="text-4xl  text-purple-800 mb-4 font-great-vibes">Vastu poojan</h1>
+        <p className="text-amber-600 mb-2 font-dancing text-2xl text-center">Join us for the inauguration of our new House.</p>
+        <a href="/rsvp" className="px-6 py-3 bg-yellow-600 text-white rounded-lg hover:bg-pink-700 transition duration-300">Open Invitation</a>
       </div>
     </div>
   );
