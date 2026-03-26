1d8d998fa7ec2ca41fd223e93965b0349b924eef
unknown <haren111990@gmail.com>
2026-03-26 14:47:36 +0530

    title and button

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,49 @@
+"use client"
+import { use, useState } from "react"
 
 
 export default function Home() {
+  const [formData, setFormData] = useState({name: "", gender: ""});
+  const handleChange=(e)=>{
+    const {name, value} = e.target;
+    setFormData((prev) => ({...prev, [name]: value}));
+  }
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    alert(`Welcome ${formData.name}, you are invited to the Vastu poojan!`);
+  }
   return (
-    <div className="h-screen w-full p-5">
-      <div className="h-100 w-full bg-cover bg-center" 
-      style={{ backgroundImage: "url('/images/ganeshji2.png')" }}></div>
+    <div className="h-screen w-full p-2">
+      <div className="h-100 w-full bg-cover bg-center"
+        style={{ backgroundImage: "url('/images/ganeshji2.png')" }}></div>
       <div className="flex flex-col items-center justify-center mt-1">
-        <h1 className="text-4xl  text-purple-800 mb-4 font-great-vibes">Vastu poojan</h1>
-        <p className="text-amber-600 mb-2 font-dancing text-2xl text-center">Join us for the inauguration of our new House.</p>
-        <a href="/rsvp" className="px-6 py-3 bg-yellow-600 text-white rounded-lg hover:bg-pink-700 transition duration-300">Open Invitation</a>
+        <h1 className="text-4xl text-pink-800 mb-1 font-great-vibes">Vastu poojan</h1>
+        <p className="text-orange-500 mb-2 font-dancing text-2xl text-center">Join us for the inauguration of our new House.</p>
+        {/* ---------------------------Form---------------------------- */}
+        <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center">
+          <input type="text"className="mt-1 p-1 border border-gray-300 rounded-lg w-full max-w-sm"
+          placeholder="Your Name" 
+          name="name" 
+          onChange={handleChange} />
+          <div>
+            <label className="block mt-2 text-sm text-gray-700">
+              <input 
+              type="radio" 
+              name="gender" 
+              value="male" 
+              onChange={handleChange}/> Male
+            </label>
+
+            <label className="block mt-2 text-sm text-gray-700">
+              <input 
+              type="radio" 
+              name="gender" 
+              value="female" 
+              onChange={handleChange}/> Female
+            </label>
+          </div>
+          <button type="submit" className="mt-2 px-4 py-2 bg-orange-400 text-white rounded-lg hover:bg-pink-700 transition duration-300">Open Invitation</button>
+        </form>
       </div>
     </div>
   );
