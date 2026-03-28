929bc5bae8f8a964021cd67240e74ef2a81d5f01
unknown <haren111990@gmail.com>
2026-03-28 20:38:33 +0530

    add location

--- app/maincard/page.tsx
+++ app/maincard/page.tsx
@@ -2,7 +2,7 @@
 
 import { useContext, useEffect, useRef } from "react";
 import { DataContext } from "@/contaxtAPI/contextApi";
-
+import { motion } from "framer-motion";
 const Page = () => {
   const { userName, gender } = useContext(DataContext);
   const audioRef = useRef<any>(null);
@@ -14,7 +14,10 @@ const Page = () => {
   }, []);
 
   return (
-    <div className="w-full h-200 relative overflow-hidden">
+    <motion.div className="w-full h-200 relative overflow-hidden"
+      initial={{ opacity: 0, y: -100 }}   
+      animate={{ opacity: 1, y: 0 }}      
+      transition={{ duration: 1 }}>
 
       {/* 🎥 Background Video */}
       <video
@@ -38,11 +41,16 @@ const Page = () => {
       {/* -------------------------name of user--------------------------------------- */}
       {/* ✨ Overlay Text */}
       <div className="absolute inset-0 flex items-center justify-center">
-        <h1 className="text-white text-3xl font-bold text-center bg-black/40 px-6 py-3 rounded-xl">
+        <motion.h1
+          initial={{ opacity: 0, y: 80, scale: 0.8 }}
+          animate={{ opacity: 1, y: 0, scale: 1 }}
+          transition={{ duration: 1 }}
+          className="text-white text-3xl font-bold text-center bg-black/40 px-6 py-3 rounded-xl"
+        >
           {userName
             ? `Hello ${gender === "male" ? "Mr." : "Miss"} ${userName}, you are invited to the Vastu poojan`
             : "Oops"}
-        </h1>
+        </motion.h1>
       </div>
       {/* -------------------------address----------------------------- */}
       <div className="absolute flex flex-col justify-center items-center bottom-10 w-full">
@@ -50,12 +58,12 @@ const Page = () => {
           <p className="text-gray-700 sm:font-semibold">Our New Addreass</p>
           <p className="text-gray-800 text-center">74,shivshakti park society,near Chaudhari samaj vadi ,musa road</p>
         </div>
-         <div className="border-[6px] border-gray-800/60 rounded-lg">
+        <div className="border-[6px] border-gray-800/60 rounded-lg">
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10528.465593096736!2d73.38571250251644!3d21.09762227475814!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be07f80c2ec6345%3A0xddad74394e2241a2!2sShiv%20Shakti%20Park%2CMusa%20Road!5e0!3m2!1sen!2sin!4v1774701008053!5m2!1sen!2sin" width="250" height="100"></iframe>
         </div>
       </div>
       {/* -------------------------bottom----------------------------- */}
-    </div>
+    </motion.div>
   );
 };
 
