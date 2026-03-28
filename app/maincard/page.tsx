b683a03383dcfbd04509b97f25971fc63ef13b0d
unknown <haren111990@gmail.com>
2026-03-28 19:47:25 +0530

    add location

--- app/maincard/page.tsx
+++ app/maincard/page.tsx
@@ -24,7 +24,7 @@ const Page = () => {
         playsInline
         className="absolute top-0 left-0 w-full h-full object-cover"
       >
-        <source src="/images/fram.mp4" type="video/mp4" />
+        <source src="/images/fram2.mp4" type="video/mp4" />
       </video>
 
       {/* 🔊 Music */}
